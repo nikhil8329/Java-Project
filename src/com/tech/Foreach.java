15dfadaa4b92fd80782a47ad1d800ba6b84302e2
nikhil8329 <walsanenik123@gmail.com>
2023-11-04 10:54:15 +0530

    Initial commit

---
+++ src/com/tech/Foreach.java
@@ -0,0 +1,18 @@
+package com.tech;
+
+import java.util.ArrayList;
+
+public class Foreach {
+
+	public static void main(String[] args) {
+	int arr[]= {11,22,33,44,55};
+		ArrayList arr1 = new ArrayList();
+		for(int arr11=0;arr11<=10;arr11++) {
+	
+		for(int i : arr)
+		{
+			System.out.println(i);
+		}
+	}
+}
+}
